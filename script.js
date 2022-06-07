ef626d2ad856481899152b4fba2b1f2b9b5d06b2
Adam Hemming <hemming.dev@gmail.com>
2022-06-07 09:40:46 +0900

    Add basic UI with RPS functionality

--- script.js
+++ script.js
@@ -29,6 +29,43 @@ if (playerSelection === "rock" && computerSelection === "paper") {
 }
 }
 
+let result;
+let playerScore = 0;
+let computerScore = 0;
+const score = document.getElementById("score");
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+    result = playRound(button.id, computerPlay());
+
+    if (result.includes("Win")) {
+        playerScore++;
+    } else if (result.includes("Lose")) {
+        computerScore++;
+    }
+
+    score.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+    console.log(`Player: ${playerScore} Computer: ${computerScore}`);
+
+    if (playerScore === 3 || computerScore === 3) {
+        const verdict = document.createElement("p");
+        const game = document.querySelector("#game");
+        verdict.classList.add("verdict");
+        if (playerScore > computerScore) {
+            verdict.textContent = "Congratulations! You beat the computer in this best of 5!";
+            game.appendChild(verdict);
+        } else if (playerScore < computerScore) {
+            verdict.textContent = "Too bad, you lost to the computer in this best of 5!"; 
+            game.appendChild(verdict);
+        } else {
+            verdict.textContent = "This best of 5 is a tie!";
+            game.appendChild(verdict);
+        }
+    }
+    });
+});
+
 function game() {
     let playerSelection;
     let computerSelection;
@@ -37,7 +74,8 @@ function game() {
     let computerScore = 0;
 
     for (let i = 0; i < 5; i++) {
-        playerSelection = window.prompt("Choose your weapon. Rock, Paper or Scissors.")
+        playerSelection = window.prompt("Choose your weapon. Rock, Paper or Scissors.");
+
         computerSelection = computerPlay();
         result = playRound(playerSelection.toLowerCase(), computerSelection);
 
@@ -63,5 +101,3 @@ function game() {
         console.log("This best of 5 is a tie!");
     }
 }
-
-game();
